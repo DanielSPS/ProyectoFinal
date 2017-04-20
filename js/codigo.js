121cb80b1a8d9ea2f13d872b0a5e30b713cc3bdf
Arturosarmiento <arturosarmientopaszor@gmail.com>
2017-04-20 10:51:52 +0200

    correcciones

---
+++ js/codigo.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	/*document.getElementsByClassName('tamaño').mouseout(categoria);*/
+});
+
+
+function categoria (){
+	document.getElementsByClassName('tamaño').fadeTo(0.15);
+}
